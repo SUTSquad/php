8739f39596da5ddce8942b619f2da8c8ca096f63
1032397987 <59812534+1032397987@users.noreply.github.com>
2020-07-22 23:37:29 +0800

    Update logincheck.php

--- 吴佳奇/logincheck.php
+++ 吴佳奇/logincheck.php
@@ -7,22 +7,35 @@ $mysqlpassword="wu7615007.";
 $mysqlusername="root";
 $dbname="regist";
 if($username==$_COOKIE['user'])
-echo "已登陆，无需登陆";
+    echo "已登陆，无需登陆";
 else
 {try{
     $connect=new PDO("mysql:host=$mysqlservername;dbname=$dbname",$mysqlusername,$mysqlpassword);
     $connect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $sth=$connect->prepare("SELECT* FROM register where username= ? and password=?");
-    $sth->bindValue(1,$username,PDO::PARAM_STR);
-    $sth->bindValue(2,$password,PDO::PARAM_STR);
-    $sth->execute();
-    $res=$sth->fetchAll();
-    if($res) {
-        echo "登陆成功";
-        setcookie("user", "$username", time() + 600);
+    $sth1=$connect->prepare("SELECT* FROM register where username= ?");
+    $sth1->bindValue(1,$username,PDO::PARAM_STR);
+
+    $sth1->execute();
+    $res1=$sth1->fetchAll();
+    if($res1) {
+
+
+        $sth2=$connect->prepare("SELECT* FROM register where username=? and password=?");
+        $sth2->bindValue(1,$username,PDO::PARAM_STR);
+        $sth2->bindValue(2,$password,PDO::PARAM_STR);
+        $sth2->execute();
+        $res2=$sth2->fetchAll();
+        if($res2)
+        {echo"登陆成功";
+            setcookie("user", "$username", time() + 600);
+
+        }
+        else
+            echo "密码错误";
+
     }
     else
-        echo"登录失败";
+        echo"用户名不存在";
 
 
 }
