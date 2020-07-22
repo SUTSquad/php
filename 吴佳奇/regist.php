4c5f6c3d2577bd840031f5950170fe86c829dcec
1032397987 <59812534+1032397987@users.noreply.github.com>
2020-07-22 14:39:58 +0800

    regist.php

---
+++ 吴佳奇/regist.php
@@ -0,0 +1,44 @@
+
+<form action="regist.php" method="post">
+
+
+
+    账号: <input type="text" name="username">
+
+
+
+    密码: <input type="password" name="password">
+
+
+
+<input type="submit" value="提交">
+
+
+
+</form>
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+$mysqlservername='39.101.167.180';
+$mysqlpassword="wu7615007.";
+$mysqlusername="root";
+$dbname="regist";
+try {
+    $connect = new PDO("mysql:host=$mysqlservername;dbname=$dbname", $mysqlusername, $mysqlpassword);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sth = $connect->prepare("INSERT INTO register (username,password)values (?,?)");
+    $sth->bindValue(1, $username, PDO::PARAM_STR);
+    $sth->bindValue(2, $password, PDO::PARAM_STR);
+    $sth->execute();
+    if ($sth)
+        echo "注册成功";
+}
+catch (PDOException $e)
+{echo "注册失败";
+
+
+
+
+
+}
+$connect=null;?>
